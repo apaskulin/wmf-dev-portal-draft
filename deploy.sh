f948db8a11fdbb6ebde32c8fc25ff65115928fad
apaskulin <apaskulin@wikimedia.org>
2021-07-19 12:02:22 -0700

    try another deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Before running this script, make sure you have:
+# * run `npm run build` from the docs directory
+# * committed your changes to the main branch
+
+# Recreate the gh-pages branch to deploy to GitHub Pages
+git branch -D gh-pages
+git checkout -b gh-pages
+
+# Copy the build files into the root so GitHub Pages can find them
+cp -r docs/src/.vuepress/dist/* .
+
+# Commit and push changes
+git add .
+git commit -m :shipit:
+git push origin gh-pages --force
