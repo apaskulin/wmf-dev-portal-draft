34519f18d584c716d828939829e42eb46fca4735
apaskulin <apaskulin@wikimedia.org>
2021-09-27 10:44:31 -0700

    add build step to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-# Before running this script, make sure you have:
-# * run `npm run build` from the docs directory
-# * committed your changes to the main branch
+# This script deploys the latest version of the main branch
+# to GitHub pages via the gh-pages branch.
+
+# Before running this script, make sure you have committed
+# your changes to the main branch.
+
+# Make sure the main branch is checked out
+git checkout main
+
+# Build the site
+cd docs
+npm run build
+
+# Commit the build files to keep things simple in the main branch
+git add .
+git commit -m :shipit:
+git push origin main
+cd ..
 
 # Recreate the gh-pages branch to deploy to GitHub Pages
 git branch -D gh-pages
